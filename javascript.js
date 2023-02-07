5bdbed4f8668ea2adc8a2ba62515c145df3aebbf
Bildurre <egoiastiz@gmail.com>
2023-02-07 13:33:50 +0100

    final js, styling start

--- javascript.js
+++ javascript.js
@@ -1,88 +1,120 @@
-const roundButton = document.querySelector("#roundButton");
+const matchScreen = document.querySelector(".matchScreen");
+const roundSelection = document.querySelector(".roundSelection");
+const roundButton = document.querySelector(".roundButton");
 
 let roundValue;
-const matchScore = {human: 0, computer: 0};
+const matchScore = {human: 0, computer: 0, round: 1};
 
 roundButton.addEventListener('click', () => { 
-  getRoundValue(); 
+  if (!isNaN(getRoundValue())) {
+    roundValue = Math.ceil(getRoundValue() / 2);
+    matchScreen.classList.toggle("hidden");
+    roundSelection.classList.toggle("hidden");
+  };
   });
   
 const weaponButtons = document.querySelectorAll(".weapon > img");
 for (const weaponButton of weaponButtons) {
   weaponButton.addEventListener('click', () => {
     playRound(Number(weaponButton.id));
-  })
-}
+  });
+};
 
-
+const playAgainButton = document.querySelector(".playAgain");
+playAgainButton.addEventListener('click', () => {
+  document.location.reload();
+});
 
 
 function getRoundValue() {
   const round = document.querySelector('input[name="round"]:checked');
   if (round == null) {
-    const showError = document.querySelector("#roundError");
+    const showError = document.querySelector(".roundError");
     if (showError == null) {
       const roundError = document.createElement("p");
-      roundError.setAttribute("id","roundError");
+      roundError.setAttribute("class","roundError");
       roundError.textContent = "You must select best of how many rounds you want to play!";
       roundError.style.color = "red";
-      document.querySelector("#roundSelection").insertBefore(roundError, roundButton);
-    }
+      document.querySelector(".roundSelection").insertBefore(roundError, roundButton);
+    };
   } else {
-    roundValue = round.value;
-  }
-}
+    return round.value;
+  };
+};
 
 function playRound(humanChoice) {
   const computerChoice = Math.floor(Math.random() * 3);
   let resultMod = humanChoice - computerChoice;
   if (resultMod < 0) {
     resultMod = resultMod + 3;
-  }
+  };
+  
   switch (resultMod) {
     case 0:
-      document.querySelector("#roundWinner").textContent = 'It\'s a draw!';
+        document.querySelector(".roundWinner").textContent = 'Round ' + matchScore.round + ': It\'s a draw!';
+      matchScore.round += 1;
       break;
     case 1:
-      document.querySelector("#roundWinner").textContent = 'You win!';
+        document.querySelector(".roundWinner").textContent = 'Round ' + matchScore.round + ': You win!';
       matchScore.human += 1;
+      matchScore.round += 1;
       break;
     case 2:
-      document.querySelector("#roundWinner").textContent = 'Computer wins!';
+        document.querySelector(".roundWinner").textContent = 'Round ' + matchScore.round + ': Computer wins!';
       matchScore.computer += 1;
+      matchScore.round += 1;
       break;
-  }
-  const humanImg = document.querySelector("#lastHumanImg");
-  switch (humanChoice) {
-    case 0:
-      humanImg.setAttribute("src", "./images/rock.png");
-      humanImg.setAttribute("alt", "rock");
-      break;
-    case 1:
-      humanImg.setAttribute("src", "./images/paper.png");
-      humanImg.setAttribute("alt", "paper");
-      break;
-    case 2:
-      humanImg.setAttribute("src", "./images/scissors.png");
-      humanImg.setAttribute("alt", "scissors");
-      break;
-  }
-  const computerImg = document.querySelector("#lastComputerImg");
-  switch (computerChoice) {
-    case 0:
-      computerImg.setAttribute("src", "./images/rock.png");
-      computerImg.setAttribute("alt", "rock");
-      break;
-    case 1:
-      computerImg.setAttribute("src", "./images/paper.png");
-      computerImg.setAttribute("alt", "paper");
-      break;
-    case 2:
-      computerImg.setAttribute("src", "./images/scissors.png");
-      computerImg.setAttribute("alt", "scissors");
-      break;
-  }
+  };
+  const humanImg = document.querySelector(".lastHumanImg");
+  showImage (humanImg, humanChoice);
   
-  document.querySelector("#humanScore").textContent = matchScore.human;
-  document.querySelector("#computerScore").textContent = matchScore.computer;
-}
+  const computerImg = document.querySelector(".lastComputerImg");
+  showImage(computerImg, computerChoice);
+ 
+  const humanNode = document.querySelectorAll(".humanScore");
+  for (const node of humanNode){
+    node.textContent = matchScore.human;
+  };
+  const computerNode = document.querySelectorAll(".computerScore");
+  for (const node of computerNode){
+    node.textContent = matchScore.computer;
+  };
+
+  if (gameEnd(roundValue,matchScore.human, matchScore.computer )){
+    document.querySelector(".weapons").classList.toggle("hidden");
+    document.querySelector(".choices").classList.toggle("hidden");
+    document.querySelector(".roundResult").classList.toggle("hidden");
+    document.querySelector(".matchEnd").classList.toggle("hidden");
+    if (matchScore.human > matchScore.computer) {
+      document.querySelector(".matchEndWinner").textContent = "Human Wins!";
+    } else {
+      document.querySelector(".matchEndWinner").textContent = "Computer Wins!";
+    }
+  }
+};
+
+function showImage(img, choice) {
+  switch (choice) {
+    case 0:
+      img.setAttribute("src", "./images/rock.png");
+      img.setAttribute("alt", "rock");
+      break;
+    case 1:
+      img.setAttribute("src", "./images/paper.png");
+      img.setAttribute("alt", "paper");
+      break;
+    case 2:
+      img.setAttribute("src", "./images/scissors.png");
+      img.setAttribute("alt", "scissors");
+      break;
+  };
+};
+
+function gameEnd(limit, humanScore, computerScore) {
+  if (humanScore >= limit || computerScore >= limit) {
+    return true;
+  } else {
+    return false;
+  };
+};
+
